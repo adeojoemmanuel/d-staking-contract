cec8e64682571ebb34d365578eb1b5b4bbc0c2dc
adeojoiemmanuel <emmanuel@gradientfi.com>
2024-03-17 06:46:24 +0500

    added token stake

--- programs/utils/src/lib.rs
+++ programs/utils/src/lib.rs
@@ -3,13 +3,16 @@ pub mod utils;
 use anchor_spl::associated_token::AssociatedToken;
 use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
 use crate::utils::resize_account;
+mod errors;
 
 // This is your program's public key and it will update
 // automatically when you build the project.
 declare_id!("2jWG6JY5EuDjFu3KHuJt4BHr433B6e8FV1LtQGXZgNqF");
 
-pub const STAKE_POOL_DEFAULT_SIZE: usize = 8 + 1 + 32 + 8 + 32 + 32 + 24;
 pub const STAKE_POOL_PREFIX: &str = "stake-pool";
+pub const STAKE_POOL_DEFAULT_SIZE: usize = 8 + 1 + 32 + 16 + 16 + 32 + 16 + 16 + 32 + 1 + 24 + 24;
+pub const STAKE_ENTRY_PREFIX: &str = "stake-entry";
+pub const STAKE_ENTRY_SIZE: usize = 8 + std::mem::size_of::<StakeEntry>() + 8;
 
 #[program]
 mod dyme_staking {
@@ -21,14 +24,26 @@ mod dyme_staking {
             bump,
             authority: ix.authority,
             total_staked: 0,
-            min_stake_seconds: ix.min_stake_seconds,
-            stake_payment_info: ix.stake_payment_info,
-            allowed_creators: ix.allowed_creators,
-            reward_amount: ix.reward_amount,
-            reward_seconds: ix.reward_seconds,
+            // min_stake_seconds: ix.min_stake_seconds,
+            token_address: ix.token_address,
+            apr: ix.apr,
+            end_date: ix.end_date,
+            pool_name: ix.pool_name,
+            default_multiplier: ix.default_multiplier,
+            created_at: Clock::get().unwrap().unix_timestamp,
             is_active: true,
             identifier,
         };
+
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.payer_token_account.to_account_info(),
+            to: ctx.accounts.pool_token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        token::transfer(cpi_ctx, ix.amount)?;
 
         let stake_pool = &mut ctx.accounts.stake_pool;
         let new_space = new_stake_pool.try_to_vec()?.len() + 8;
@@ -41,14 +56,52 @@ mod dyme_staking {
         )?;
 
         stake_pool.set_inner(new_stake_pool);
+
         Ok(())
     }
 
-    pub fn update_pool(ctx: Context<UpdatePoolCtx>, ix: UpdatePoolIx) -> Result<()> {
-        let stake_pool = &mut ctx.accounts.stake_pool;
-        stake_pool.min_stake_seconds = ix.min_stake_seconds;
+    pub fn init_stake_entry(ctx: Context<InitEntryCtx>) -> Result<()> {
+        let stake_entry = &mut ctx.accounts.stake_entry;
+        stake_entry.bump = ctx.bumps.stake_entry;
+        stake_entry.pool = ctx.accounts.stake_pool.key();
+        stake_entry.stake_mint = ctx.accounts.stake_mint.key();
+        stake_entry.amount = 0;
         Ok(())
     }
+
+    pub fn stake_token(ctx: Context<InitStakeCtx>, ix: InitStakeIx) -> Result<()> {
+        let stake_entry = &mut ctx.accounts.stake_entry;
+        let stake_pool = &mut ctx.accounts.stake_pool;
+        if stake_pool.end_date.is_some()
+            && Clock::get().unwrap().unix_timestamp > stake_pool.end_date.unwrap()
+        {
+            return Err(errors::ErrorCode::StakePoolHasEnded.into());
+        }
+
+
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.payer_token_account.to_account_info(),
+            to: ctx.accounts.entry_token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        token::transfer(cpi_ctx, ix.amount)?;
+
+        stake_entry.last_staker = ctx.accounts.payer.key();
+        stake_entry.last_staked_at = Clock::get().unwrap().unix_timestamp;
+        stake_entry.amount = stake_entry.amount.checked_add(ix.amount).unwrap();
+        stake_entry.min_stake_seconds = ix.min_stake_seconds;
+        stake_pool.total_staked = stake_pool.total_staked.checked_add(1).expect("Add error");
+        Ok(())
+    }
+
+    // pub fn update_pool(ctx: Context<UpdatePoolCtx>, ix: UpdatePoolIx) -> Result<()> {
+    //     let stake_pool = &mut ctx.accounts.stake_pool;
+    //     stake_pool.min_stake_seconds = ix.min_stake_seconds;
+    //     Ok(())
+    // }
 }
 
 #[derive(Accounts)]
@@ -72,6 +125,8 @@ pub struct InitPoolCtx<'info> {
     token_address: Account<'info, Mint>,
 
     #[account(mut)]
+    payer_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
     payer: Signer<'info>,
     token_program: Program<'info, Token>,
     system_program: Program<'info, System>,
@@ -81,8 +136,59 @@ pub struct InitPoolCtx<'info> {
 
 #[derive(Accounts)]
 pub struct UpdatePoolCtx<'info> {
-    #[account(mut)]
+    #[account(mut, constraint = stake_pool.authority == payer.key())]
     stake_pool: Account<'info, StakePool>,
+    #[account(mut)]
+    payer: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitEntryCtx<'info> {
+    #[account(
+        init,
+        payer = payer,
+        space = STAKE_ENTRY_SIZE,
+        seeds = [STAKE_ENTRY_PREFIX.as_bytes(), stake_pool.key().as_ref(), stake_mint.key().as_ref()],
+        bump,
+    )]
+    stake_entry: Box<Account<'info, StakeEntry>>,
+    #[account(mut)]
+    stake_pool: Box<Account<'info, StakePool>>,
+    #[account(
+        init_if_needed,
+        payer = payer, 
+        associated_token::mint = stake_mint, 
+        associated_token::authority = stake_entry
+    )]
+    entry_token_account: Account<'info, TokenAccount>,
+    stake_mint: Box<Account<'info, Mint>>,
+    token_program: Program<'info, Token>,
+    associated_token_program: Program<'info, AssociatedToken>,
+    rent: Sysvar<'info, Rent>,
+
+    #[account(mut)]
+    payer: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitStakeCtx<'info> {
+    #[account(mut)]
+    stake_entry: Box<Account<'info, StakeEntry>>,
+    #[account(mut)]
+    stake_pool: Box<Account<'info, StakePool>>,
+    #[account(mut)]
+    entry_token_account: Account<'info, TokenAccount>,
+
+    stake_mint: Box<Account<'info, Mint>>,
+
+    #[account(mut)]
+    payer_token_account: Account<'info, TokenAccount>,
+    token_program: Program<'info, Token>,
+    associated_token_program: Program<'info, AssociatedToken>,
+    rent: Sysvar<'info, Rent>,
+
     #[account(mut)]
     payer: Signer<'info>,
     system_program: Program<'info, System>,
@@ -93,25 +199,46 @@ pub struct StakePool {
     pub bump: u8,
     pub authority: Pubkey,
     pub total_staked: u32,
-    pub min_stake_seconds: Option<u32>,
-    pub stake_payment_info: Pubkey,
-    pub allowed_creators: Vec<Pubkey>,
-    pub reward_amount: u32,
-    pub reward_seconds: Option<u32>,
+    // pub min_stake_seconds: Option<u32>,
+    pub token_address: Pubkey,
+    pub apr: u64,
+    pub end_date: Option<i64>,
     pub is_active: bool,
     pub identifier: String,
+    pub pool_name: String,
+    pub default_multiplier: u64,
+    pub created_at: i64,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize)]
 pub struct InitPoolIx {
-    allowed_creators: Vec<Pubkey>,
     authority: Pubkey,
-    min_stake_seconds: Option<u32>,
-    stake_payment_info: Pubkey,
-    reward_amount: u32,
-    reward_seconds: Option<u32>,
+    // min_stake_seconds: Option<u32>,
+    token_address: Pubkey,
+    apr: u64,
     is_active: bool,
+    end_date: Option<i64>,
     identifier: String,
+    pool_name: String,
+    default_multiplier: u64,
+    amount: u64,
+}
+
+#[account]
+pub struct StakeEntry {
+    pub bump: u8,
+    pub pool: Pubkey,
+    pub amount: u64,
+    pub stake_mint: Pubkey,
+    pub last_staker: Pubkey,
+    pub last_staked_at: i64,
+    pub min_stake_seconds: Option<u32>,
+}
+
+#[account]
+pub struct InitStakeIx {
+    pub amount: u64,
+    pub min_stake_seconds: Option<u32>,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize)]
